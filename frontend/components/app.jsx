9532b5c77c6e313ebafe77550b9931ec13073978
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-18 13:55:48 -0700

    Add greeting and session containers

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, HashRouter, Link, Switch, Redirect } from 'react-router-dom';
-
+import SessionFormContainer from './session_form/session_form_container';
+import GreetingContainer from './greeting/greeting_container';
 
 const App = () => (
   <div>
     <header>
       <Link to="/" className="header-link">
-        <h1>Behance</h1>
+        <h1>Baehance</h1>
       </Link>
+      <GreetingContainer />
     </header>
-
-</div>
+    <Route path="/login" component={SessionFormContainer} />
+    <Route path="/signup" component={SessionFormContainer} />
+  </div>
 );
 
 export default App;
