23cdee644e4c7d17df185b77eca1ac01bd17e914
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-25 11:30:23 -0700

    Add image selector

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,5 +1,4 @@
 import { values } from 'lodash';
 
 export const selectAllProjects = ({ projects }) => values(projects);
-//unsure about image selection
 export const selectImages = ({ images }) => values(images);
