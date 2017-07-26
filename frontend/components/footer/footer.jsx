7d5c4a4565a1318cb35d770a2b8184b8f9385828
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-25 20:03:26 -0700

    Add icons to footer, style user page

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,43 +3,17 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer">
     <section className="foot-left">
-      <h1>About</h1>
-      <p>Baehance is a portfolio showcase web application inspired by Behance.</p>
+      <p>Baehance is a portfolio showcase web application inspired by Behance, made by Anastassia.</p>
     </section>
     <section className="foot-right">
-      <a href="https://github.com/anastassia-b">Github</a>
-      <a href="https://www.linkedin.com/in/anastassia-bobokalonova-mit">LinkedIn</a>
-      <a href="mailto:email.anastassia@gmail.com">Contact</a>
+      <a href="https://github.com/anastassia-b">
+        <i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
+      <a href="https://www.linkedin.com/in/anastassia-bobokalonova-mit">
+        <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+      <a href="mailto:email.anastassia@gmail.com">
+        <i className="fa fa-envelope-square fa-2x" aria-hidden="true"></i></a>
     </section>
   </footer>
 );
 
 export default Footer;
-
-// import React from 'react';
-// import {Link} from 'react-router-dom';
-//
-// const Footer = () => {
-//   return (
-//     <footer className="footer">
-//       <a href="https://github.com/virginiac32/full-stack-project">
-//       <i className="fa fa-github fa-lg" aria-hidden="true"></i>
-//       </a>
-//       <a href="https://www.linkedin.com/in/virginia-chen/">
-//       <i className="fa fa-linkedin-square fa-lg" aria-hidden="true"></i>
-//       </a>
-//     </footer>
-//   );
-// };
-//
-// export default Footer;
-
-
-// <footer className="auth-footer">
-//   <a href="https://github.com/ade713" target="_blank">
-//     <i className="fa fa-github fa-2x" aria-hidden="true"></i>
-//   </a>
-//   <a href="https://www.linkedin.com/in/ade-farquhar-2a66a233" target="_blank">
-//     <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
-//   </a>
-// </footer>
