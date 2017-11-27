5072f944993380f05ca8f6a7f2b613910369e754
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-11-27 13:14:18 -0800

    Import logger only in development

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from '../middleware/thunk_middleware';
 import RootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger'
 
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
+  import logger from 'redux-logger';
   middlewares.push(logger);
 }
 
