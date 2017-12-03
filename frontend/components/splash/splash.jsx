7f67ce58fe3dfb1a47180ba5886c500c70cb1f42
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-12-03 12:43:14 -0800

    Delete login scroll

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,23 +5,19 @@ class Splash extends React.Component {
   constructor(props) {
     super(props);
     this.guestLogin = this.guestLogin.bind(this);
-    this.handleScroll = this.handleScroll.bind(this);
   }
 
   guestLogin(e) {
     e.preventDefault();
     const user = {username: 'guest', password: 'password'};
     this.props.login(user);
-    this.handleScroll();
   }
 
-  handleScroll() {
-    let scroll = Scroll.animateScroll;
-    scroll.scrollTo(900, {
-      duration: 1500,
-      delay: 50,
-      smooth: true
-    });
+  demoButton() {
+    return (
+      <button className="session-button"
+              onClick={this.guestLogin}>Demo</button>
+    )
   }
 
   render() {
@@ -30,8 +26,7 @@ class Splash extends React.Component {
         <section className="splash-text">
           <h1>Art Portfolio</h1>
           <h4>All your favorite art, all in one place. Showcase creative work and discover pieces from past and present.</h4>
-          <button className="session-button"
-                  onClick={this.guestLogin}>Demo</button>
+          { demoButton() }
         </section>
       </section>
     );
