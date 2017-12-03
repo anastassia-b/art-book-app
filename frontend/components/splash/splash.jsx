386db4e1846000a5ff0a81100b87e90876cc2178
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-12-03 12:50:47 -0800

    Refactor button based on currentUser

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -14,10 +14,13 @@ class Splash extends React.Component {
   }
 
   demoButton() {
+    const name = this.props.currentUser.username;
+    if (this.props.currentUser) {
+      return (<h4>Welcome, {name}.</h4>)
+    } else {
     return (
-      <button className="session-button"
-              onClick={this.guestLogin}>Demo</button>
-    )
+      <button className="session-button" onClick={this.guestLogin}>Demo</button>
+    )}
   }
 
   render() {
@@ -26,7 +29,7 @@ class Splash extends React.Component {
         <section className="splash-text">
           <h1>Art Portfolio</h1>
           <h4>All your favorite art, all in one place. Showcase creative work and discover pieces from past and present.</h4>
-          { demoButton() }
+          { this.demoButton() }
         </section>
       </section>
     );
--- frontend/components/splash/splash_container.js
+++ frontend/components/splash/splash_container.js
@@ -3,11 +3,14 @@ import { connect } from 'react-redux';
 import { login } from '../../actions/session_actions';
 import Splash from './splash';
 
+const mapStateToProps = ({ session }) => ({
+  currentUser: session.currentUser
+});
 const mapDispatchToProps = (dispatch) => ({
   login: user => dispatch(login(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Splash);
