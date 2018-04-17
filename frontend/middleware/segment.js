e9f3e4b965ffa515ff4972b2033c70f8ed2eac20
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-04-17 13:24:01 -0700

    Add Segment middleware

---
+++ frontend/middleware/segment.js
@@ -0,0 +1,40 @@
+import { RECEIVE_COMMENT } from '../actions/comment_actions';
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+
+const Segment = store => next => action => {
+    let eventProperties = {};
+
+    switch(action.type) {
+        case RECEIVE_COMMENT:
+            eventProperties: {
+                comment: action.comment
+            }
+            analytics.track(
+                'Create Comment', eventProperties
+            );
+            return next(action);
+
+        case RECEIVE_CURRENT_USER:
+            eventProperties: {
+                currentUser: action.currentUser
+            }
+            analytics.track(
+                'Login User', eventProperties
+            );
+            return next(action);
+
+        case RECEIVE_ERRORS:
+            eventProperties = {
+                errors: action.errors
+            }
+            analytics.track(
+                'Receive Errors', eventProperties
+            );
+            return next(action);
+
+        default:
+            return next(action);
+    }
+}
+
+export default Segment;
